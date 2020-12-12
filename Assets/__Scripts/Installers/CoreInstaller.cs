4b89f6d1257ede9f0c417b6da0dbe2cb48035eae
Caeden117 <caeden.s@outlook.com>
2020-12-12 10:36:12 -0800

    Create basic Core Installer

---
+++ Assets/__Scripts/Installers/CoreInstaller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class CoreInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        // Read https://github.com/svermeulen/Extenject documentation for what exactly is going on.
+        Container.Bind<Settings>().AsSingle().NonLazy();
+    }
+}
